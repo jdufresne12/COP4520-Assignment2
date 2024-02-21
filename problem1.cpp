6c868e146ffd75e5e0f1524cf36ada7f6ab43783
jdufresne12 <jdufresne135@gmail.com>
2024-02-20 21:10:30 -0500

    INIT

---
+++ problem1.cpp
@@ -0,0 +1,9 @@
+/*
+    John Dufresne 
+    COP 4520
+    Problem 1: Minotaur's Birthday Party
+        A minotaur invites N guests to his birthday party. They all arrive, but can only go in one at a time, when he calls them.
+        He may call the same guest multiple times. Guests announce they have all entered the labyrinth, so that the minotaur is aware.
+        Now the guests must come up with a strategy to let the Minotaur know that every guest entered the Minotaur’s labyrinth.
+*/
+
