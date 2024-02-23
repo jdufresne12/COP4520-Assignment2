ad46e0d873f7c1ad7b5d346399d87d8b607b17a1
jdufresne12 <jdufresne135@gmail.com>
2024-02-22 19:24:58 -0500

    Problem 2 Init

---
+++ problem2.cpp
@@ -0,0 +1,10 @@
+/*
+    John Dufresne 
+    COP 4520
+    Problem 2: Minotaur’s Crystal Vase
+        The Minotaur decided to show his favorite crystal vase to his guests in a dedicated
+        showroom with a single door. He did not want many guests to gather around the vase
+        and accidentally break it. For this reason, he would allow only one guest at a time into
+        the showroom. He asked his guests to choose from one of three possible strategies for
+        viewing the Minotaur’s favorite crystal vase:
+*/
